fb66bb7183b22a977d59925ca7b81b64e8319e3f
Cedric Weber <cedric.weber@stackit.cloud>
2025-09-15 08:32:46 +0200

    fix: just params inheritance

--- justfile
+++ justfile
@@ -1,20 +1,53 @@
 set dotenv-load
 set dotenv-filename := ".env"
     
-check:
+bump *ARGS:
+    dagger call bump \
+        --repository-url=$REPOSITORY_URL \
+        --user=$USER \
+        --git-token=env:GIT_TOKEN {{ ARGS }}
+
+changelog *ARGS:
+    dagger call changelog \
+        --repository-url=$REPOSITORY_URL \
+        --user=$USER \
+        --git-token=env:GIT_TOKEN {{ ARGS }}
+
+check *ARGS:
     dagger call check \
         --repository-url=$REPOSITORY_URL \
         --user=$USER \
-        --git-token=env:GIT_TOKEN
+        --git-token=env:GIT_TOKEN {{ ARGS }}
 
-install-hooks:
+commit *ARGS:
+    dagger call commit \
+        --repository-url=$REPOSITORY_URL \
+        --user=$USER \
+        --git-token=env:GIT_TOKEN {{ ARGS }}
+
+get-version *ARGS:
+    dagger call get-version \
+        --repository-url=$REPOSITORY_URL \
+        --user=$USER \
+        --git-token=env:GIT_TOKEN {{ ARGS }}
+
+install-hooks *ARGS:
     dagger call install-hooks \
         --repository-url=$REPOSITORY_URL \
         --user=$USER \
-        --git-token=env:GIT_TOKEN
+        --git-token=env:GIT_TOKEN {{ ARGS }}
 
-verify:
+list:
+    dagger functions
+
+log *ARGS:
+    dagger call log \
+        --repository-url=$REPOSITORY_URL \
+        --user=$USER \
+        --git-token=env:GIT_TOKEN {{ ARGS }}
+
+verify *ARGS:
     dagger call verify \
         --repository-url=$REPOSITORY_URL \
         --user=$USER \
-        --git-token=env:GIT_TOKEN
+        --git-token=env:GIT_TOKEN {{ ARGS }}
--- main.go
+++ main.go
@@ -9,7 +9,15 @@ import (
 type DagCoco struct{}
 
 // Base Function returning a rust latest container with cocogitto installed
-func (m *DagCoco) Base(ctx context.Context, repositoryUrl string, user string, gitToken *dagger.Secret) *dagger.Container {
+func (m *DagCoco) Base(
+	// current context
+	ctx context.Context,
+	// remote repository url
+	repositoryUrl string,
+	// user account name
+	user string,
+	// user account git token
+	gitToken *dagger.Secret) *dagger.Container {
 	repository := m.CloneRepository(ctx, user, repositoryUrl, gitToken).Directory("/repository")
 
 	return dag.Container().
@@ -19,21 +27,265 @@ func (m *DagCoco) Base(ctx context.Context, repositoryUrl string, user string, g
 		WithExec([]string{"cargo", "install", "--locked", "cocogitto"})
 }
 
-// Get the current version
-// https://docs.cocogitto.io/guide/misc.html
-func (m *DagCoco) GetVersion(ctx context.Context, repositoryUrl string, user string, gitToken *dagger.Secret, options *GetVersionOptions) (string, error) {
+// Bump version
+// https://docs.cocogitto.io/guide/bump.html
+func (m *DagCoco) Bump(
+	// context
+	ctx context.Context,
+	// remote repository URL
+	repositoryUrl string,
+	// remote username
+	user string,
+	// secret Git token
+	gitToken *dagger.Secret,
+	// +optional
+	// auto bump version
+	auto bool,
+	// +optional
+	// cogToml file reference
+	cogToml *dagger.File,
+	// +optional
+	// should run dry
+	dryRun bool,
+	// +optional
+	// increment major
+	major bool,
+	// +optional
+	// increment minor
+	minor bool,
+	// +optional
+	// increment patch
+	patch bool,
+	// +optional
+	// metadata before building
+	preMeta string,
+	// +optional
+	// build metadata
+	buildMeta string,
+	// +optional
+	// set specfic version (prio over major,minor,patch)
+	version string,
+	// +optional
+	// Skip ci flag
+	skipCi bool,
+) (string, error) {
 
 	args := ""
-	if options != nil {
-		if options.Package != nil {
-			args = fmt.Sprintf("--package %s", *options.Package)
+	base := m.Base(ctx, repositoryUrl, user, gitToken)
+
+	if version == "" {
+		if auto {
+			args = "--auto"
 		}
-		if options.Fallback != nil {
-			args = fmt.Sprintf("%s --fallback %s", args, *options.Fallback)
+
+		if major {
+			args = fmt.Sprintf("%s --major", args)
 		}
-		if options.Silence {
-			args = fmt.Sprintf("%s -v", args)
+
+		if minor {
+			args = fmt.Sprintf("%s --minor", args)
 		}
+
+		if patch {
+			args = fmt.Sprintf("%s --patch", args)
+		}
+	} else {
+		args = fmt.Sprintf("--version %s", version)
+	}
+
+	if cogToml != nil {
+		base.WithFile("cog.toml", cogToml)
+	}
+
+	if dryRun {
+		args = fmt.Sprintf("%s --dry-run", args)
+	}
+
+	if preMeta != "" {
+		args = fmt.Sprintf("%s --pre \"%s\"", args, preMeta)
+	}
+
+	if buildMeta != "" {
+		args = fmt.Sprintf("%s --build \"%s\"", args, buildMeta)
+	}
+
+	if skipCi {
+		args = fmt.Sprintf("%s --skip-ci", args)
+	}
+
+	return base.
+		WithExec([]string{"sh", "-c", fmt.Sprintf("cog bump %s", args)}).
+		Stdout(ctx)
+}
+
+// Generate a changelog automatically
+// https://docs.cocogitto.io/guide/changelog.html
+func (m *DagCoco) Changelog(
+	// context vontext
+	ctx context.Context,
+	// remote repo address
+	repositoryUrl string,
+	// remote user account
+	user string,
+	// secret git token
+	gitToken *dagger.Secret, version string,
+	// +optional
+	// changelog between 1.00 and at
+	at string,
+	// +optional
+	// changelog template: default, full_hash, remote
+	template string,
+	// +optional
+	// remote domain: e.g. google.com
+	remote string,
+	// +optional
+	// user account / organization
+	owner string,
+	// +optional
+	// repository name
+	repository string,
+) (string, error) {
+
+	args := ""
+
+	if at != "" {
+		args = fmt.Sprintf("--at %s", at)
+	}
+
+	if template != "" {
+		args = fmt.Sprintf("%s --template %s", args, template)
+
+		if template == "remote" {
+
+			if remote != "" {
+				args = fmt.Sprintf("%s --remote %s", args, remote)
+			}
+
+			if owner != "" {
+				args = fmt.Sprintf("%s --owner %s", args, owner)
+			}
+
+			if repository != "" {
+				args = fmt.Sprintf("%s --repository %s", args, repository)
+			}
+
+		}
+	}
+
+	return m.Base(ctx, repositoryUrl, user, gitToken).
+		WithExec([]string{"sh", "-c", fmt.Sprintf("cog changelog %s %s", args, version)}).
+		Stdout(ctx)
+
+}
+
+// Check commit history against conventional commits spec
+// https://docs.cocogitto.io/guide/check.html
+func (m *DagCoco) Check(
+	// current context
+	ctx context.Context,
+	// remote repository url
+	repositoryUrl string,
+	// user account
+	user string,
+	// remote git token secret
+	gitToken *dagger.Secret,
+	// +optional
+	cogToml *dagger.File) (string, error) {
+
+	base := m.Base(ctx, repositoryUrl, user, gitToken)
+
+	if cogToml != nil {
+		base = base.WithFile("cog.toml", cogToml)
+	}
+
+	return base.
+		WithExec([]string{"sh", "-c", "cog check"}).
+		Stdout(ctx)
+}
+
+// Create a new conventional commit using coco
+// https://docs.cocogitto.io/guide/commit.html
+func (m *DagCoco) Commit(
+	// current context
+	ctx context.Context,
+	// remote repo url
+	repositoryUrl string,
+	// user account
+	user string,
+	// repository secret token
+	gitToken *dagger.Secret,
+	// +optional
+	// Cocogitto config file
+	cogToml *dagger.File,
+	// +optional
+	// tyoe of commit
+	commitType string,
+	// +optional
+	// commit message
+	commitMessage string,
+	// +optional
+	// is it a breaking change
+	breakingChange bool,
+) (string, error) {
+
+	base := m.Base(ctx, repositoryUrl, user, gitToken)
+	args := ""
+
+	if commitType == "" {
+		return "", fmt.Errorf("invalid commit type")
+	}
+
+	args = commitType
+
+	if breakingChange {
+		args = fmt.Sprintf("%s -B", args)
+	}
+
+	if commitMessage == "" {
+		return "", fmt.Errorf("commit message can not be empty")
+	}
+
+	args = fmt.Sprintf("%s \"%s\"", args, commitMessage)
+
+	if cogToml != nil {
+		base.WithFile("cog.toml", cogToml)
+	}
+
+	return base.WithExec([]string{"sh", "-c", fmt.Sprintf("cog commit %s", args)}).Stdout(ctx)
+}
+
+// Get the current version
+// https://docs.cocogitto.io/guide/misc.html
+func (m *DagCoco) GetVersion(
+	// current context
+	ctx context.Context,
+	// remote repo URL
+	repositoryUrl string,
+	// username
+	user string,
+	// secret git token
+	gitToken *dagger.Secret,
+	// +optional
+	// fallback version
+	fallback string,
+	// +optional
+	// define package name
+	pack string,
+	// +optional
+	// Only get version
+	silence bool,
+) (string, error) {
+
+	args := ""
+
+	if pack != "" {
+		args = fmt.Sprintf("--package %s", pack)
+	}
+	if fallback != "" {
+		args = fmt.Sprintf("%s --fallback %s", args, fallback)
+	}
+	if silence {
+		args = fmt.Sprintf("%s -v", args)
 	}
 
 	return m.Base(ctx, repositoryUrl, user, gitToken).
@@ -41,52 +293,19 @@ func (m *DagCoco) GetVersion(ctx context.Context, repositoryUrl string, user str
 		Stdout(ctx)
 }
 
-// Create a new conventional commit using coco
-// https://docs.cocogitto.io/guide/commit.html
-func (m *DagCoco) Commit(ctx context.Context, repositoryUrl string, user string, gitToken *dagger.Secret, options *CommitOptions) (string, error) {
-
-	base := m.Base(ctx, repositoryUrl, user, gitToken)
-	args := ""
-
-	if options == nil {
-		return "", fmt.Errorf("invalid commit options")
-	}
-
-	if options.CommitType == "" {
-		return "", fmt.Errorf("invalid commit type")
-	}
-
-	args = options.CommitType
-
-	if options.BreakingChange {
-		args = fmt.Sprintf("%s -B", args)
-	}
-
-	if options.CommitMessage == "" {
-		return "", fmt.Errorf("commit message can not be empty")
-	}
-
-	args = fmt.Sprintf("%s \"%s\"", args, options.CommitMessage)
-
-	if options.CogToml != nil {
-		base.WithFile("cog.toml", options.CogToml)
-	}
-
-	return base.WithExec([]string{"sh", "-c", fmt.Sprintf("cog commit %s", args)}).Stdout(ctx)
-}
-
-// Check commit history against conventional commits spec
-// https://docs.cocogitto.io/guide/check.html
-func (m *DagCoco) Check(ctx context.Context, repositoryUrl string, user string, gitToken *dagger.Secret) (string, error) {
-
-	return m.Base(ctx, repositoryUrl, user, gitToken).
-		WithExec([]string{"sh", "-c", "cog check"}).
-		Stdout(ctx)
-}
-
 // Install a single or all hooks
 // https://docs.cocogitto.io/guide/git_hooks.html
-func (m *DagCoco) InstallHooks(ctx context.Context, repositoryUrl string, user string, gitToken *dagger.Secret, commits *string) (string, error) {
+func (m *DagCoco) InstallHooks(
+	// the current context
+	ctx context.Context,
+	// remote repo URL
+	repositoryUrl string,
+	// username
+	user string,
+	// git secret token
+	gitToken *dagger.Secret,
+	// semver, commit, range commit xxx..xxy
+	commits *string) (string, error) {
 
 	args := ""
 	if commits != nil {
@@ -100,45 +319,59 @@ func (m *DagCoco) InstallHooks(ctx context.Context, repositoryUrl string, user s
 		Stdout(ctx)
 }
 
-// Checks input string against conventional commits specification, error if not a conv commit
-// https://docs.cocogitto.io/guide/verify.html
-func (m *DagCoco) Verify(ctx context.Context, repositoryUrl string, user string, gitToken *dagger.Secret, commitMsg string) (string, error) {
-	return m.Base(ctx, repositoryUrl, user, gitToken).
-		WithExec([]string{"sh", "-c", fmt.Sprintf("cog verify \"%s\"", commitMsg)}).
-		Stdout(ctx)
-}
-
 // Get cocogitto log (like custom git log)
 // https://docs.cocogitto.io/guide/log.html
-func (m *DagCoco) Log(ctx context.Context, repositoryUrl string, user string, gitToken *dagger.Secret, version string, options *LogOptions) (string, error) {
+func (m *DagCoco) Log(
+	// current context
+	ctx context.Context,
+	// repository remote address
+	repositoryUrl string,
+	// username
+	user string,
+	// git token
+	gitToken *dagger.Secret,
+	// +optional
+	// filter authors
+	authors []string,
+	// +optional
+	// define commit type: e.g. feat
+	commitType string,
+	// +optional
+	// scope of commit
+	scope string,
+	// +optional
+	// no error enforce
+	noError bool,
+	// +optional
+	// filter breaking changes only
+	breakingOnly bool,
+) (string, error) {
 
 	args := ""
-	if options != nil {
-		if options.Authors != nil {
-			if len(options.Authors) != 0 {
-				args = "--author"
-				for _, author := range options.Authors {
-					args = fmt.Sprintf("%s \"%s\"", args, author)
-				}
+
+	if authors != nil {
+		if len(authors) != 0 {
+			args = "--author"
+			for _, author := range authors {
+				args = fmt.Sprintf("%s \"%s\"", args, author)
 			}
 		}
+	}
 
-		if options.BreakingOnly {
-			args = fmt.Sprintf("%s -B", args)
-		}
+	if breakingOnly {
+		args = fmt.Sprintf("%s -B", args)
+	}
 
-		if options.CommitType != nil {
-			args = fmt.Sprintf("%s --type %s", args, *options.CommitType)
-		}
+	if commitType != "" {
+		args = fmt.Sprintf("%s --type %s", args, commitType)
+	}
 
-		if options.Scope != nil {
-			args = fmt.Sprintf("%s --scope %s", args, *options.Scope)
-		}
+	if scope != "" {
+		args = fmt.Sprintf("%s --scope %s", args, scope)
+	}
 
-		if options.NoError {
-			args = fmt.Sprintf("%s --no-error", args)
-		}
-
+	if noError {
+		args = fmt.Sprintf("%s --no-error", args)
 	}
 
 	return m.Base(ctx, repositoryUrl, user, gitToken).
@@ -147,94 +380,20 @@ func (m *DagCoco) Log(ctx context.Context, repositoryUrl string, user string, gi
 
 }
 
-// Generate a changelog automatically
-// https://docs.cocogitto.io/guide/changelog.html
-func (m *DagCoco) Changelog(ctx context.Context, repositoryUrl string, user string, gitToken *dagger.Secret, version string, options *ChangelogOptions) (string, error) {
-
-	args := ""
-	if options != nil {
-		if options.At != nil {
-			args = fmt.Sprintf("--at %s", *options.At)
-		}
-
-		if options.Template != nil {
-			args = fmt.Sprintf("%s --template %s", args, *options.Remote)
-
-			if *options.Template == "remote" {
-
-				if options.Remote != nil {
-					args = fmt.Sprintf("%s --remote %s", args, *options.Owner)
-				}
-
-				if options.Owner != nil {
-					args = fmt.Sprintf("%s --owner %s", args, *options.Owner)
-				}
-
-				if options.Repository != nil {
-					args = fmt.Sprintf("%s --repository %s", args, *options.Repository)
-				}
-
-			}
-		}
-	}
+// Checks input string against conventional commits specification, error if not a conv commit
+// https://docs.cocogitto.io/guide/verify.html
+func (m *DagCoco) Verify(
+	// current context
+	ctx context.Context,
+	// repository url
+	repositoryUrl string,
+	// username
+	user string,
+	// git token secret
+	gitToken *dagger.Secret,
+	// message to check
+	commitMsg string) (string, error) {
 	return m.Base(ctx, repositoryUrl, user, gitToken).
-		WithExec([]string{"sh", "-c", fmt.Sprintf("cog changelog %s %s", args, version)}).
-		Stdout(ctx)
-
-}
-
-// Bump version
-// https://docs.cocogitto.io/guide/bump.html
-func (m *DagCoco) Bump(ctx context.Context, repositoryUrl string, user string, gitToken *dagger.Secret, options *VersionBumpOptions) (string, error) {
-
-	args := ""
-	base := m.Base(ctx, repositoryUrl, user, gitToken)
-
-	if options != nil {
-
-		if options.Version == nil {
-			if options.Auto {
-				args = "--auto"
-			}
-
-			if options.Major {
-				args = fmt.Sprintf("%s --major", args)
-			}
-
-			if options.Minor {
-				args = fmt.Sprintf("%s --minor", args)
-			}
-
-			if options.Patch {
-				args = fmt.Sprintf("%s --patch", args)
-			}
-		} else {
-			args = fmt.Sprintf("--version %s", *options.Version)
-		}
-
-		if options.CogToml != nil {
-			base.WithFile("cog.toml", options.CogToml)
-		}
-
-		if options.DryRun {
-			args = fmt.Sprintf("%s --dry-run", args)
-		}
-
-		if *options.PreMeta != "" {
-			args = fmt.Sprintf("%s --pre \"%s\"", args, *options.PreMeta)
-		}
-
-		if *options.BuildMeta != "" {
-			args = fmt.Sprintf("%s --build \"%s\"", args, *options.BuildMeta)
-		}
-
-		if options.SkipCi {
-			args = fmt.Sprintf("%s --skip-ci", args)
-		}
-
-	}
-
-	return base.
-		WithExec([]string{"sh", "-c", fmt.Sprintf("cog bump %s", args)}).
+		WithExec([]string{"sh", "-c", fmt.Sprintf("cog verify \"%s\"", commitMsg)}).
 		Stdout(ctx)
 }
